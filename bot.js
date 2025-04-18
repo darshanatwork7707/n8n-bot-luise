4a9b963aa39b3e79b3c3680afdf3d6f8db93977c
DARSHAN M S <darshanatwork7707@gmail.com>
2025-04-18 14:01:58 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-const fetch = require('node-fetch');  // For CommonJS (if using require)
 const { Client, GatewayIntentBits } = require('discord.js');
 const fetch = require('node-fetch');
 require('dotenv').config();
